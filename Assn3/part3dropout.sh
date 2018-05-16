b33177bf653ab3a0fe976bf07c4d461bc93e5323
Arthur Shing <arthur.shing@gmail.com>
2018-05-15 18:39:42 -0700

    Added prob 3 and prob 4 code and pics

---
+++ Assn3/part3dropout.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python prob3dropout.py 1
+python prob3dropout.py 0
+python prob3dropout.py 0.001
+python prob3dropout.py 0.01
+python prob3dropout.py 0.1
+python prob3dropout.py 0.3
+python prob3dropout.py 0.5
+python prob3dropout.py 0.7
+python prob3dropout.py 0.9
---
+++ Assn3/part3weightdecay.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python prob3weightdecay.py 0.1
+python prob3weightdecay.py 0.01
+python prob3weightdecay.py 0.001
+python prob3weightdecay.py 0.0001
